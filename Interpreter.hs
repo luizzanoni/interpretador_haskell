f05b7ee87cb42561893ab2d17a0221d223e1cf23
Luiz Gustavo Zanoni <luizgustavo@interfoc.com.br>
2024-12-05 14:54:43 -0300

    ajustes e refatoração do código

---
+++ Interpreter.hs
@@ -0,0 +1,55 @@
+module Interpreter where
+
+import Lexer 
+
+isValue :: Expr -> Bool
+isValue BTrue = True 
+isValue BFalse = True 
+isValue (Num _) = True 
+isValue (Lam _ _ _) = True
+isValue (Tuple _ _) = True  -- Reconhecendo tuplas como valores
+isValue _ = False 
+
+subst :: String -> Expr -> Expr -> Expr
+subst x n b@(Var v) = if v == x then 
+                        n 
+                      else 
+                        b 
+subst x n (Add e1 e2) = Add (subst x n e1) (subst x n e2)
+subst x n (Sub e1 e2) = Sub (subst x n e1) (subst x n e2)  -- Substituição para a operação de subtração
+subst x n (And e1 e2) = And (subst x n e1) (subst x n e2)
+subst x n (Eq e1 e2) = Eq (subst x n e1) (subst x n e2)
+subst x n (If e e1 e2) = If (subst x n e) (subst x n e1) (subst x n e2)
+subst x n (Lam v t b) = Lam v t (subst x n b)
+subst x n (App e1 e2) = App (subst x n e1) (subst x n e2)
+subst x n (Tuple e1 e2) = Tuple (subst x n e1) (subst x n e2)  -- Substituição para tuplas
+subst _ _ e = e
+
+step :: Expr -> Expr 
+step (Add (Num n1) (Num n2)) = Num (n1 + n2)
+step (Sub (Num n1) (Num n2)) = Num (n1 - n2)  -- Passo para a operação de subtração
+step (Add (Num nv) e2) = Add (Num nv) (step e2) 
+step (Sub (Num nv) e2) = Sub (Num nv) (step e2)  -- Passo para a operação de subtração
+step (Add e1 e2) = Add (step e1) e2 
+step (Sub e1 e2) = Sub (step e1) e2  -- Passo para subtração
+step (And BFalse e) = BFalse 
+step (And BTrue e) = e 
+step (And e1 e2) = And (step e1) e2 
+
+step (Eq e1 e2) | isValue e1 && isValue e2 = if (e1 == e2) then 
+                                               BTrue 
+                                             else 
+                                               BFalse
+                | isValue e1 = Eq e1 (step e2)
+                | otherwise = Eq (step e1) e2
+step (If BTrue e1 e2) = e1 
+step (If BFalse e1 e2) = e2 
+step (If e e1 e2) = If (step e) e1 e2 
+step (App (Lam v t b) e) | isValue e = subst v e b 
+                         | otherwise = (App (Lam v t b) (step e))
+step (App e1 e2) = App (step e1) e2
+step (Tuple e1 e2) = Tuple (step e1) (step e2)  -- Passo para tuplas
+
+eval :: Expr -> Expr 
+eval e | isValue e = e 
+       | otherwise = eval (step e) 
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# interpretador_haskell
+# hs-interpreter-24-2
+
+para instalar o GHCup, rodar o comando no PowerShell como adm
+
+`Set-ExecutionPolicy Bypass -Scope Process -Force;[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; try { & ([ScriptBlock]::Create((Invoke-WebRequest https://www.haskell.org/ghcup/sh/bootstrap-haskell.ps1 -UseBasicParsing))) -Interactive -DisableCurl } catch { Write-Error $_ }`
+
+depois de instalar ele, deve colocar nas variávels de ambiente o ghc,
+colocar de preferência nas variáveis do windows e NÃO do usuário
+
+`C:\ghcup\ghc\9.4.8\bin`
+
